ce80d073dfa85d77d9cfd39a698bc1f27f5584a2
ashfill <ashton@ASHTON-PC>
2016-10-12 11:15:59 -0700

    homework.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string filename = "C:\\Users\\ashton\\Documents\\week3day3.txt";
+
+            //List<string1> c = new List<string1>();
+
+            string1 d = new string1();
+            d.Number = "The number is {0}";
+
+            StreamWriter gh = new StreamWriter(filename, true);
+           
+            for (int i = 0; i < 100; i++)
+            {
+                string1 g = new string1();
+                g.Number = string.Format("The number is {0}", i + 1);
+                gh.Write(g.Number);
+              
+            }
+            gh.Close();
+
+            StreamReader yt = new StreamReader(filename);
+            Console.ReadKey();
+        }
+            }
+        }
+    
+
---
+++ ConsoleApplication1/ConsoleApplication1/string.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+   public class string1
+    {
+        private string number;
+
+        public string Number
+        {
+            get
+            {
+                return number;
+            }
+
+            set
+            {
+                number = value;
+            }
+        }
+    }
+}
